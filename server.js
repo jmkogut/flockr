e7e53692a9657a61b6246e926b3e7e1058c5ef4f
Joshua Mikkael <dottru@gmail.com>
2013-09-05 03:32:28 +0000

    changed mmo.html to flockin

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ var app = require('http').createServer(handler),
     app.listen( 8080 );
 
 io.static.add('two.js', {file: 'lib/two.js/build/two.js'});
+io.static.add('dat.gui.js', {file: 'lib/dat.gui/build/dat.gui.js'});
 io.static.add('util.js', {file: 'lib/util.js'});
 io.static.add('keypress.js', {file: 'lib/keypress/keypress.js'});
+io.static.add('underscore.js', {file: 'lib/underscore/underscore.js'});
+io.static.add('flock.js', {file: 'lib/flock.js'});
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
